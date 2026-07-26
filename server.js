49d2b75ba7c2ec1c4cae0d675e0fdffe593ec382
baran <25093463d@connect.polyu.hk>
2026-07-26 18:52:42 +0300

    feat(auth): thread userId into room peer objects

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function generateRoomCode() {
 
 function addPeerToRoom(socket, code, name) {
     socket.emit('existing-peers', { peers : rooms[code].peers, code })
-    rooms[code].peers[socket.id] = { name }
+    rooms[code].peers[socket.id] = { name, userId: socket.userId }
     socket.join(code)
     console.log(name, 'entered room no:', code)
     socket.code = code
